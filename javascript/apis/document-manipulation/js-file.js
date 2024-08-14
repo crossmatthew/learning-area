d3c5e3b30222b34bc4f1544cb4b89f13b13d0c18
matthew <mashh@live.co.uk>
2024-08-14 20:54:59 +0100

    handleButtonClick function with event listeners for clicks and keypress

--- javascript/apis/document-manipulation/js-file.js
+++ javascript/apis/document-manipulation/js-file.js
@@ -1,4 +1,31 @@
 const ul = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+let inputValue = input.value;
 
+function handleButtonClick() {
+    let inputValue = input.value;
+    input.value = '';
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const btn = document.createElement('button');
+    li.appendChild(span);
+    li.appendChild(btn);
+    ul.appendChild(li);
+    span.textContent = inputValue;
+    btn.textContent = 'Delete';
+    btn.addEventListener('click', () => {
+        ul.removeChild(li);
+    })
+    input.focus();
+};
+
+button.addEventListener('click', () => {
+    handleButtonClick();
+})
+
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        handleButtonClick();
+    };
+});
